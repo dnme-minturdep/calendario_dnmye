61eb1e3b3a7a5c008a788c609a218106b253e509
Juan Pablo Ruiz Nicolini <jruiznicolini@turismo.gob.ar>
2022-08-25 15:34:19 -0300

    boceto de GHA para actualizar CALENDARIO

---
+++ .github/source/check_calendar_items.R
@@ -0,0 +1,33 @@
+install.packages("googlesheets4")
+install.packages("dplyr")
+install.packages("stringr")
+intall.packages("rmarkdwon")
+
+library(googlesheets4)
+library(stringr)
+library(dplyr)
+library(rmarkdown)
+
+
+googlesheets4::gs4_deauth()
+
+calendario_drive <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/11nyQ5QwuTUaS6F_g8ZM6Ta9SXnrOiCtPV3h2rG_LVV0/edit#gid=0") %>% 
+  filter(if_any(.cos = everything(), 
+                .fns = ~ !is.na(.))) %>% 
+  nrow()
+
+
+
+calendario <- read.csv("https://raw.githubusercontent.com/dnme-minturdep/calendario_dnmye/main/.github/data/n_published.csv") %>% 
+  pull()
+
+
+if(calendario_drive > calendario){
+  
+  rmarkdown::render(input = "index.Rmd", output_yaml = "_site.yml")
+
+  } else {
+  
+  print("No hay nuevas publicaciones")
+  }
+
