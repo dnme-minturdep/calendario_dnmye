7711489bb5a1711cff2f172f7e5b7694a4c8b56a
eliansoutu <eliansoutul@gmail.com>
2023-12-12 16:02:17 -0300

    actualizo

--- .github/source/check_calendar_items.R
+++ .github/source/check_calendar_items.R
@@ -12,12 +12,12 @@ calendario_drive <- googlesheets4::read_sheet("https://docs.google.com/spreadshe
 calendario <- read.csv("https://raw.githubusercontent.com/dnme-minturdep/calendario_dnmye/main/.github/data/n_published.csv") %>% 
   pull()
 
-if(calendario_drive != calendario){
+#if(calendario_drive != calendario){
   
   rmarkdown::render(input = "index.Rmd", output_dir = "docs" , output_yaml = "_site.yml")
 
-  } else {
+ # } else {
   
-  print("No hay nuevas publicaciones")
-  }
+  #print("No hay nuevas publicaciones")
+  #}
 
