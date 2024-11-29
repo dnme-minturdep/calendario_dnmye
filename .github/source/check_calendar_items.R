8f1c248bdd4f8dca5fdccf5706d64c2fa7d464f9
eliansoutu <eliansoutul@gmail.com>
2024-11-29 11:00:38 -0300

    ajusto condicion rmd

--- .github/source/check_calendar_items.R
+++ .github/source/check_calendar_items.R
@@ -5,8 +5,7 @@ googlesheets4::gs4_deauth()
 path <- Sys.getenv("CALENDAR_PATH")
 
 calendario_drive <- googlesheets4::read_sheet(path) %>% 
-  filter(if_any(.cos = everything(), 
-                .fns = ~ !is.na(.))) %>% 
+  drop_na() %>% 
   mutate(across(everything(), as.character))
 
 
@@ -14,7 +13,7 @@ calendario_drive <- googlesheets4::read_sheet(path) %>%
 calendario <- read.csv("https://raw.githubusercontent.com/dnme-minturdep/calendario_dnmye/main/.github/data/calendario_publicado.csv") %>% 
   mutate(across(everything(), as.character))
 
-if(all_equal(calendario, calendario_drive) == F){
+if(identical(calendario, calendario_drive) == F){
   
   rmarkdown::render(input = "index.Rmd", output_dir = "docs" , output_yaml = "_site.yml")
 
